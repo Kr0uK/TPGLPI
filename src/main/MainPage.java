b62965bcf4c7a046c1112ed2662a7edc071bb74c
Kr0uK <boris.motz@gmail.com>
2016-10-12 14:08:04 +0200

    .

--- src/main/MainPage.java
+++ src/main/MainPage.java
@@ -3,7 +3,12 @@ package main; /**
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainPage extends Application {
 
@@ -12,7 +17,12 @@ public class MainPage extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
     }
-}
+
