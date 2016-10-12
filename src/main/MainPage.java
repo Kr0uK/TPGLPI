713fde9728cc39b7faf1eb4d84482ff5bd8b8ff7
Kr0uK <boris.motz@gmail.com>
2016-10-12 13:56:36 +0200

    .

---
+++ src/main/MainPage.java
@@ -0,0 +1,18 @@
+package main; /**
+ * Created by user on 12/10/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainPage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
